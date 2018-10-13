58fed9864169fa1cabd10f2d708b0106d1e64526
David P <sholwa55@gmail.com>
2018-10-13 15:03:41 +0200

    Kezdeti program fájlok létrehozása
    főprogram szerkezetének tervezése

---
+++ rPI_music_box/main.py
@@ -0,0 +1,28 @@
+# függvénykönyvtárak betöltése
+#       senseHat (eszköz tesztelése)
+#       zene lejátszás
+
+# mappa(ák) fájl(ok) meglétének ellenőrzése
+import os
+
+#print(os.path.isdir("C:\Users\dpazsitni\Documents\GitHub\mems-beadando\rPI_music_box"))
+if os.path.isdir("c:/Users/dpazsitni/Documents/GitHub/mems-beadando/rPI_music_box/music") == True:
+    print("The folder exists")
+else:
+    print("The folder is not exists")
+
+# Várakozás parancsra
+#while expression:
+#    pass
+
+#Fügvények
+    #play
+
+    #pause
+
+    #next
+
+    #previous
+
+    #Schuffle
+
