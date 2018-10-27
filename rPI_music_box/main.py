f2c4ee9cce52d13d8f48fc9d018ed083901fb48b
David P <sholwa55@gmail.com>
2018-10-27 18:03:43 +0200

    zene lejátszási lehetőség

--- rPI_music_box/main.py
+++ rPI_music_box/main.py
@@ -1,19 +1,35 @@
 # függvénykönyvtárak betöltése
 #       senseHat (eszköz tesztelése)
 #       zene lejátszás
-
-# mappa(ák) fájl(ok) meglétének ellenőrzése
+import vlc
+# mappa(ák) fájl(ok) kezelése
 import os
+# from os.path import join
+
 
 #print(os.path.isdir("C:\Users\dpazsitni\Documents\GitHub\mems-beadando\rPI_music_box"))
-if os.path.isdir("c:/Users/dpazsitni/Documents/GitHub/mems-beadando/rPI_music_box/music") == True:
+path="c:/Users/dpazsitni/Documents/GitHub/mems-beadando/rPI_music_box/music"
+if os.path.isdir(path) == True:
     print("The folder exists")
+    l=os.listdir(path)
+    
+    mp3=os.path.join(path,l[0])
+    #onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+    
+    player=vlc.MediaPlayer(mp3)
+    player.play()
+    
+    input()
+    from time import sleep
+    sleep(10)
+    player.stop()
+    
 else:
     print("The folder is not exists")
 
 # Várakozás parancsra
 #while expression:
-#    pass
+ #   pass
 
 #Fügvények
     #play
@@ -25,4 +41,8 @@ else:
     #previous
 
     #Schuffle
+
+    #Display
+
+
 
